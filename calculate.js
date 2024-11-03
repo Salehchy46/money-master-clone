17c204c66e7d86da7db24d07f925a1374db64cdc
Salehchy46 <salehchyctg@gmail.com>
2024-11-03 23:27:45 +0600

    my monthly salary calculation

--- calculate.js
+++ calculate.js
@@ -1,3 +1,5 @@
+//salary calculation
+
 function moneyCalculation(monthlySalary){
   const amountInput = document.getElementById(monthlySalary);
   const amountValue = amountInput.value;
@@ -5,6 +7,13 @@ function moneyCalculation(monthlySalary){
   return amount;
 }
 
+//the addition of expense and extra
+function addTextIn (elementId, others){
+  const element = document.getElementById(elementId);
+  element.innerText = others;
+}
+
+//total expenses
 function expensesMonthly (){
   const foodBill = moneyCalculation('food-bill');
   const houseRent = moneyCalculation('house-bill');
@@ -14,13 +23,41 @@ function expensesMonthly (){
   return totalExpenses;
 }
 
+//Salary
 function salaryCalculation (){
   const salary = moneyCalculation('income-amount');
   return salary;
 }
 
-document.getElementById('calculate').addEventListener('click', function(){
-  const calculated = salaryCalculation() - expensesMonthly();
-  console.log(calculated);
+//total extra
+function extendedMoney (){
+  const extraMoney = salaryCalculation() - expensesMonthly();
+  return extraMoney;
+}
+
+//savings
+function floatingExtendedMoney(){
+  const savedFunc = extendedMoney();
+  const saved = parseFloat(savedFunc);
+  return saved;
+}
+
+function monthlySavings (){
+  const saveMoney = 0.8 * floatingExtendedMoney();
+  return saveMoney;
+}
+
+function afterSavings (){
+  const residualMoney = extendedMoney() - monthlySavings();
+  return residualMoney;
+}
+document.getElementById('calculate').addEventListener('click', function (){
+  addTextIn('total-expense', expensesMonthly());
+  addTextIn('remainder', extendedMoney());
+})
+
+document.getElementById('save-money').addEventListener('click', function(){
+  addTextIn('money-saved', monthlySavings());
+  addTextIn('residual', afterSavings());
 })
 
